6025cfb82b91bdec5872583fcfc67920f6359b5a
SathyamGoyal-GP <sathyam.goyal@greenlightplanet.com>
2023-01-19 18:13:49 +0530

    CHECKPOINT

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { HttpParams } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 
 interface User{
   email: any;
@@ -28,30 +26,7 @@ interface User{
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit{
-  apiData: any;
-  isLoading = false;
-  email: any;
-  login_type: any;
-  password: any;
-  name: any;
-  phone: any;
-  role: any;
-  country: any;
-  zone: any;
-  region: any;
-  area: any;
-  territory: any;
-  super_user: any;
-  latitude : any;
-  longitude : any;
-  area_id: any;
-  module: any;
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {}
-
-  row = [
+  rows = [
     {
       email: '',
       login_type: '',
@@ -68,12 +43,16 @@ export class UsersComponent implements OnInit{
       area_id: '',
       latitude: '',
       longitude: '',
-      module: ''
-    }
+      module: '',
+    },
   ];
-  
-  addTable() {
-    const obj = {
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  addRow() {
+    this.rows.push({
       email: '',
       login_type: '',
       password: '',
@@ -89,70 +68,55 @@ export class UsersComponent implements OnInit{
       area_id: '',
       latitude: '',
       longitude: '',
-      module: ''
-    }
-    this.row.push(obj)
-  }
-  // saveTable() {
-  //   // this.isLoading = false;
-  //   const data = {email: this.email,login_type: this.login_type,password: this.password,name: this.name,phone: this.phone,role: this.role,country: this.country,zone: this.zone,region: this.region,area: this.area,territory: this.territory,super_user: this.super_user,area_id: this.area_id,module: this.module, latitude: this.latitude,longitude: this.longitude};
-
-  //   this.http.post('http://localhost:8000/amigo/v1.0/user-angular/', data).subscribe(response => {
-  //     console.log(response);
-  //   });
-
-    saveTable(row : any) {
-      // this.isLoading = false;
-      // const data = {email: this.email,login_type: this.login_type,password: this.password,name: this.name,phone: this.phone,role: this.role,country: this.country,zone: this.zone,region: this.region,area: this.area,territory: this.territory,super_user: this.super_user,area_id: this.area_id,module: this.module, latitude: this.latitude,longitude: this.longitude};
-  
-      this.http.post('http://localhost:8000/amigo/v1.0/user-angular/', row).subscribe(response => {
-        console.log(response);
-      });
-
-    // this.email = '';
-    // this.login_type = '';
-    // this.password = '';
-    // this.name = '';
-    // this.phone = '';
-    // this.role = '';
-    // this.country = '';
-    // this.zone = '';
-    // this.region = '';
-    // this.area = '';
-    // this.territory = '';
-    // this.super_user = '';
-    // this.area_id = '';
-    // this.latitude = '';
-    // this.longitude = '';
-    // this.module = '';
-  }
-  
-  deleteRow(x: number){
-    var delBtn = confirm(" Do you want to delete ?");
-    if ( delBtn == true ) {
-      this.row.splice(x, 1 );
-    }   
-  }
-  getData() {
-    let params = new HttpParams().set('email_id', this.email);
-    this.http.get<User>('http://localhost:8000/amigo/v1.0/workbench-tables/staff-users', {params}).subscribe(data => {
-      this.row[0] = data;
+      module: '',
     });
-    // this.email = this.apiData.email;
-    // this.login_type = this.apiData.login_type;
-    // this.password = this.apiData.password;
-    // this.name = this.apiData.name;
-    // this.phone = this.apiData.phone;
-    // this.role = this.apiData.role;
-    // this.country = this.apiData.country;
-    // this.zone = this.apiData.zone;
-    // this.region = this.apiData.region;
-    // this.area = this.apiData.area;
-    // this.territory = this.apiData.territory;
-    // this.super_user = this.apiData.super_user;
-    // this.area_id = this.apiData.area_id;
-    // this.latitude = this.apiData.latitude;
-    // this.longitude = this.apiData.longitude;
-    // this.module = this.apiData.module;
+  }
+
+  getData(index: number) {
+    const email = this.rows[index].email;
+    this.http
+      .get<{
+        email: any;
+        login_type: any;
+        password: any;
+        name: any;
+        phone: any;
+        role: any;
+        country: any;
+        zone: any;
+        region: any;
+        area: any;
+        territory: any;
+        super_user: any;
+        latitude: any;
+        longitude: any;
+        area_id: any;
+        module: any;
+      }>(
+        `http://localhost:8000/amigo/v1.0/workbench-tables/staff-users?email_id=${email}`
+      )
+      .subscribe((data) => {
+        this.rows[index] = data;
+      });
+  }
+
+  saveData(row : any) {
+    const email = row.email;
+    if (row.email) {
+      this.http
+        .put(
+          `http://localhost:8000/amigo/v1.0/workbench-tables/staff-users/?email_id=${row.email}`,
+          row
+        )
+        .subscribe();
+    } else {
+      this.http
+        .post(`http://localhost:8000/amigo/v1.0/workbench-tables/staff-users/`, row)
+        .subscribe();
+    }
+  }
+
+  deleteRow(index: number) {
+    this.rows.splice(index, 1);
   }
 }
